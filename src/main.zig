3d0debe5215751f6aabcc7a608d6d6e896b3a90b
invisibleYoubic <79036954430d@gmail.com>
2026-02-19 23:51:20 +0400

    make project structure more clear

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,9 @@
-export fn kmain() noreturn {
-    const uart_base = 0x1c00030000;
+const uart_base = 0x1c00030000;
+const UART0_DR = @as(*volatile u32, @ptrFromInt(uart_base + 0x00));
 
-    const UART0_DR = @as(*volatile u32, @ptrFromInt(uart_base + 0x00));
-
-    while (true) {
-        UART0_DR.* = 'A';
-
-        var i: u32 = 0;
-        while (i < 10_000_000) : (i += 1) {
-            std.mem.doNotOptimizeAway(i);
-        }
+fn uart_print(msg: []const u8) void {
+    for (msg) |char| {
+        UART0_DR.* = char;
     }
 }
 
@@ -17,5 +11,20 @@ const std = @import("std");
 pub fn panic(_: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
     while (true) {
         asm volatile ("wfi");
+    }
+}
+
+export fn kmain() noreturn {
+    var stack_msg = [_]u8{ 's', 't', 'a', 'c', 'k', '\r', '\n' };
+    _ = &stack_msg;
+
+    while (true) {
+        uart_print(&stack_msg);
+        uart_print("Direct message string\n\r");
+
+        var i: u32 = 0;
+        while (i < 10_000_000) : (i += 1) {
+            std.mem.doNotOptimizeAway(i);
+        }
     }
 }
